bc7d0e7059352b1555735ef85c033e62af096ad3
Eike Bartels <eike.t.bartels@gmail.com>
2015-09-06 18:24:16 +0200

    added pods

---
+++ Podfile
@@ -0,0 +1,31 @@
+# Uncomment this line to define a global platform for your project
+
+use_frameworks!
+workspace 'RealmJsonMapper'
+
+
+xcodeproj 'Example/Example.xcodeproj'
+
+target 'Example' do
+    platform :ios, '8.0'
+    pod 'RealmSwift'
+    
+    xcodeproj 'Example/Example.xcodeproj'
+end
+
+target 'RealmJsonMapper' do
+    platform :ios, '8.0'
+    pod 'RealmSwift'
+    
+    xcodeproj 'RealmJsonMapper/RealmJsonMapper.xcodeproj'
+
+end
+
+
+
+
+#target 'RealmJsonMapperTests' do
+
+#end
+
+
---
+++ RealmJsonMapper/RealmJsonMapper/RealmJsonMapper.h
@@ -0,0 +1,19 @@
+//
+//  RealmJsonMapper.h
+//  RealmJsonMapper
+//
+//  Created by Eike Bartels on 06/09/15.
+//  Copyright (c) 2015 Eike Bartels. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RealmJsonMapper.
+FOUNDATION_EXPORT double RealmJsonMapperVersionNumber;
+
+//! Project version string for RealmJsonMapper.
+FOUNDATION_EXPORT const unsigned char RealmJsonMapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RealmJsonMapper/PublicHeader.h>
+
+
